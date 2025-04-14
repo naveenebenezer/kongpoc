d4a5bb1867c0ae06d299b071a6673ce0e15a8cb9
naveenebenezer <47165099+naveenebenezer@users.noreply.github.com>
2025-04-14 15:13:28 -0400

    Update kong.sh

--- kong.sh
+++ kong.sh
@@ -1,2 +1,8 @@
 # install eks and kong
-# echo " starting to install ... "
+echo " starting to install ... "
+mkdir -p /home/ssm-user/eks/
+cd /home/ssm-user/eks/
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.3/2024-12-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
